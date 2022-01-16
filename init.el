6887438a3d1e66ef016c1a0f26ea0cdba71ff4a8
basicfunc <basicfunc@gmail.com>
2022-01-16 12:40:45 +0530

    def

--- init.el
+++ init.el
@@ -16,86 +16,65 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(straight-use-package 'use-package)
+(setq inhibit-startup-message t)
+(setq fast-but-imprecise-scrolling t)
+(setq visible-bell nil)
+(setq frame-inhibit-implied-resize t)
+(setq use-dialog-box nil)
+(setq compilation-scroll-output 'first-error)
+(setq default-frame-parameters '((inhibit-double-buffering . t)))
+(setq ad-redefinition-action 'accept)
+(setq create-locked nil)
+(setq auto-revert-use-notify nil)
+(setq split-width-threshold nil)
+(setq electric-pair-preserve-balance nil)
 
-(use-package straight
-  :custom
-  (straight-use-package-by-default t)
-  (use-package-always-ensure nil))
+(global-visual-line-mode +1)
+(setq custom-file (concat user-emacs-directory "custom.el"))
+(setq-default auto-save-default nil
+	      create-lockfiles nil
+	      make-backup-files nil
+	      cursor-type 'bar
+	      truncate-lines t
+	      word-wrap t)
 
-(use-package emacs
-  :config
-  (setq inhibit-startup-message t)
-  (setq fast-but-imprecise-scrolling t)
-  (setq visible-bell nil)
-  (setq frame-inhibit-implied-resize t)
-  (setq use-dialog-box nil)
-  (setq compilation-scroll-output 'first-error)
-  (setq default-frame-parameters '((inhibit-double-buffering . t)))
-  (setq ad-redefinition-action 'accept)
-  (setq create-locked nil)
-  (setq auto-revert-use-notify nil)
-  (setq split-width-threshold nil)
-  (setq electric-pair-preserve-balance nil)
+(straight-use-package 'no-littering)
+(require 'no-littering)
+(setq custom-file (no-littering-expand-etc-file-name "custom.el"))
 
-  (global-visual-line-mode +1)
-  (setq custom-file (concat user-emacs-directory "custom.el"))
-  (setq-default auto-save-default nil
-		create-lockfiles nil
-		make-backup-files nil
-		cursor-type 'bar
-		truncate-lines t
-		word-wrap t))
+(require 'recentf)
+(setq recentf-save-file (expand-file-name "recentf" no-littering-etc-directory)
+      recentf-max-saved-items 500
+      recentf-max-menu-items 15
+      recentf-auto-cleanup 'never)
+(add-to-list 'recentf-exclude no-littering-var-directory)
+(add-to-list 'recentf-exclude no-littering-etc-directory)
+(recentf-mode +1)
 
-(use-package no-littering
-  :custom (custom-file (no-littering-expand-etc-file-name "custom.el")))
+(setq savehist-additional-variables
+      '(search-ring regexp-search-ring)
+      savehist-autosave-interval 60
+      savehist-file (expand-file-name "savehist" no-littering-etc-directory))
+(savehist-mode +1)
 
-(use-package recentf
-  :config
-  (setq recentf-save-file (expand-file-name "recentf" no-littering-etc-directory)
-        recentf-max-saved-items 500
-        recentf-max-menu-items 15
-        recentf-auto-cleanup 'never)
-  (add-to-list 'recentf-exclude no-littering-var-directory)
-  (add-to-list 'recentf-exclude no-littering-etc-directory)
-  (recentf-mode +1))
+(electric-pair-mode +1)
 
-(use-package savehist
-  :config
-  (setq savehist-additional-variables
-        '(search-ring regexp-search-ring)
-        savehist-autosave-interval 60
-        savehist-file (expand-file-name "savehist" no-littering-etc-directory))
-  (savehist-mode +1))
+;; built-in packages
+(set-face-background 'show-paren-match "#262b36")
+(set-face-bold 'show-paren-match t)
+(set-face-foreground 'show-paren-match "#ffffff")
+(show-paren-mode +1)
 
-(use-package elec-pair
-  :config
-  (electric-pair-mode +1))
-
-;;; built-in packages
-(use-package paren
-  :config
-  (set-face-background 'show-paren-match "#262b36")
-  (set-face-bold 'show-paren-match t)
-  (set-face-foreground 'show-paren-match "#ffffff")
-  (show-paren-mode +1))
-
-(use-package display-line-numbers
-  :config
-  (setq-default display-line-numbers-grow-only t
+(setq-default display-line-numbers-grow-only t
 	      display-line-numbers-type 'relative
 	      display-line-numbers-width 2)
-  (global-display-line-numbers-mode +1))
+(global-display-line-numbers-mode +1)
 
-(use-package dired
-  :straight (:type built-in)
-  :config
-  (put 'dired-find-alternate-file 'disabled nil)
-
-  (setq dired-recursive-deletes 'always)
-  (setq dired-recursive-copies 'always)
-
-  (setq dired-dwim-target t))
+;; Dired
+(put 'dired-find-alternate-file 'disabled nil)
+(setq dired-recursive-deletes 'always)
+(setq dired-recursive-copies 'always)
+(setq dired-dwim-target t)
 
 ;; The alias to save life.
 (defalias 'yes-or-no-p 'y-or-n-p)
--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -1,45 +1,44 @@
 ;; Just Developers Things.
-(use-package button-lock)
-(use-package fixmee
-  :after button-lock
-  :init (global-fixmee-mode +1))
+(straight-use-package 'button-lock)
+(require 'button-lock)
+(straight-use-package 'fixmee)
+(global-fixmee-mode +1)
 
 
 ;; Emacs as IDE Setup.
-(use-package eglot
-  :config
-  (add-hook 'eglot--managed-mode-hook #'(lambda () (add-hook 'before-save-hook 'eglot-format nil t))))
+(straight-use-package 'eglot)
+(require 'eglot)
+(add-hook 'eglot--managed-mode-hook #'(lambda () (add-hook 'before-save-hook 'eglot-format nil t)))
 
-;; For C/C++.
-(add-to-list 'eglot-server-programs '((c-modee c++-mode) . ("clangd" "--clang-tidy")))
+;; For C & C++.
+(add-to-list 'eglot-server-programs '((c-mode c++-mode) . ("clangd" "--clang-tidy")))
 (add-hook 'c-mode-hook 'eglot-ensure)
 (add-hook 'c++-mode-hook 'eglot-ensure)
 ;; For Python.
 (add-to-list 'eglot-server-programs '((python-mode) . ("pyls")))
 (add-hook 'python-mode-hook 'eglot-ensure)
+
 ;; For Rust.
-(use-package rust-mode)
+(straight-use-package 'rust-mode)
 (add-to-list 'eglot-server-programs '((rust-mode) . ("rust-analyzer")))
 (add-hook 'rust-mode-hook 'eglot-ensure)
 
 ;; Go colorful with Tree-sitter.
-(use-package tree-sitter
-  :init (global-tree-sitter-mode)
-  :config
-  (use-package tree-sitter-langs)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
+(global-tree-sitter-mode) 
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
 
 ;; Best GIT interface ever, Magit.
-(use-package magit)
+(straight-use-package 'magit)
 
 ;; Terminal.
-(use-package vterm
-  :config
-  (use-package vterm-toggle)
-  (add-hook 'vterm-mode-hook (lambda () (display-line-numbers-mode -1))))
+(straight-use-package 'vterm)
+(straight-use-package 'vterm-toggle)
+(add-hook 'vterm-mode-hook (lambda () (display-line-numbers-mode -1)))
 
 ;; Snippets
-(use-package yasnippet-snippets
-  :init (yas-global-mode +1))
+(straight-use-package 'yasnippet-snippets)
+(yas-global-mode +1)
 
 (provide 'init-prog)
