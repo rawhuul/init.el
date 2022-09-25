78a7912c3fd6b9bcb021f4cddf278a3cd843bb4f
Rahul <basicfunc@gmail.com>
2022-09-25 16:50:04 +0530

    Update

--- init.el
+++ init.el
@@ -2,6 +2,26 @@
 
 ;; Code:
 
+(setq package-enable-at-startup nil)
+(setq native-comp-deferred-compilation t)
+(setq package-native-compile t)
+(setq gc-cons-threshold (* 150 1000 1000))
+(setq read-process-output-max (* 1024 1024))
+
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(menu-bar-mode -1)
+(setq frame-title-format (concat "ᕙ༼◕ ᴥ ◕༽ᕗ"))
+(add-to-list 'default-frame-alist '(height . 34))
+(add-to-list 'default-frame-alist '(width . 70))
+
+(set-face-attribute 'default nil
+                    :family "CascadiaCode"
+                    :height 106
+                    :weight 'normal
+                    :width 'normal)
+
 ;; Installing straight.el
 (defvar bootstrap-version)
 (let ((bootstrap-file
@@ -29,7 +49,10 @@
 (setq split-width-threshold nil)
 (setq electric-pair-preserve-balance nil)
 
+(global-hl-line-mode +1)
 (global-visual-line-mode +1)
+(global-prettify-symbols-mode +1)
+
 (setq custom-file (concat user-emacs-directory "custom.el"))
 (setq-default auto-save-default nil
 	      create-lockfiles nil
@@ -79,11 +102,153 @@
 (setq dired-recursive-copies 'always)
 (setq dired-dwim-target t)
 
+;; Ligature for fonts.
+(straight-use-package
+ '(ligature :type git :host github :repo "mickeynp/ligature.el"))
+
+(ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                     ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                     "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                     "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                     "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                     "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                     "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                     "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                     ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                     "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                     "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                     "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                     "\\\\" "://"))
+(global-ligature-mode t)
+
+;; Have some emotions XD.
+(straight-use-package 'emojify)
+
+;; Very powerful undo/redo system.
+(straight-use-package 'undo-tree)
+(global-undo-tree-mode +1)
+
+;; Highlight your indentation to make things more simple.
+(straight-use-package 'highlight-indent-guides)
+(require 'highlight-indent-guides)
+(set-face-background 'highlight-indent-guides-odd-face "darkgray")
+(set-face-background 'highlight-indent-guides-even-face "dimgray")
+(set-face-foreground 'highlight-indent-guides-character-face "dimgray")
+(setq highlight-indent-guides-method 'character)
+(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+
+;; Using icons.
+(straight-use-package 'all-the-icons)
+(straight-use-package 'all-the-icons-completion)
+(all-the-icons-completion-mode)
+(add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)
+(straight-use-package 'all-the-icons-dired)
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
+
+;; A warm theme.
+(straight-use-package 'doom-themes)
+(setq doom-themes-enable-bold t
+      doom-themes-enable-italic t)
+(load-theme 'doom-one-light t)
+
+;; OHhhh, this modeline
+(straight-use-package 'doom-modeline)
+(add-hook 'after-init-hook #'doom-modeline-mode)
+
+;; Making things a lot easy.
+(straight-use-package 'vertico)
+(vertico-mode +1)
+
+(straight-use-package 'marginalia)
+(marginalia-mode +1)
+
+;; Emacs completion style enhancement.
+(straight-use-package 'orderless)
+(setq completion-styles '(orderless))
+
+;; Commands based on the Emacs completion.
+(straight-use-package 'consult)
+
+;; Text completion framework.
+(straight-use-package 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
+(defvar my-company-point nil)
+(advice-add 'company-complete-common :before (lambda () (setq my-company-point (point))))
+(advice-add 'company-complete-common :after (lambda ()
+  		  			      (when (equal my-company-point (point))
+  			  			(yas-expand))))
+
+(setq company-minimum-prefix-length 1
+      company-ideal-dealy 0.0
+      company-tooltip-align-annotations t
+      company-selection-wrap-around t)
+(company-tng-configure-default)
+
+;; Woohh, present at multiple places at same time, like multiverse.
+(straight-use-package 'multiple-cursors)
+(multiple-cursors-mode +1)
+
+;; Auto delimiter pairing
+(add-hook 'prog-mode-hook 'electric-pair-local-mode)
+
+;; Yoo, markdown is quite simple.
+(straight-use-package 'markdown-mode)
+
+;; Emacs knocking off vscode.
+(straight-use-package 'eglot)
+(require 'eglot)
+(add-hook 'eglot--managed-mode-hook (lambda () (add-hook 'before-save-hook #'eglot-format-buffer nil nil)))
+
+;; C, forever.
+(add-hook 'c-mode-hook #'eglot-ensure)
+;; Pure, nonsense.
+(add-hook 'c++-mode-hook #'eglot-ensure)
+
+;; Feeding Snakes.
+(add-hook 'python-mode-hook #'eglot-ensure)
+
+;; Cute fiddly language.
+(straight-use-package 'rust-mode)
+(add-hook 'rust-mode-hook #'eglot-ensure)
+
+;; Go colorful with Tree-sitter.
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
+(global-tree-sitter-mode) 
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+
+;; Even code folds
+(straight-use-package
+ '(ts-fold :type git :host github :repo "emacs-tree-sitter/ts-fold"))
+(add-hook 'tree-sitter-after-on-hook #'ts-fold-indicators-mode)
+
+;; Less strokes, more completions.
+(straight-use-package 'yasnippet-snippets)
+(setq yas-also-auto-indent-first-line t)
+(setq yas-also-indent-empty-lines t)
+(yas-global-mode +1)
+
+;; What's a developer without a terminal.
+(straight-use-package 'vterm)
+(straight-use-package 'vterm-toggle)
+(add-hook 'vterm-mode-hook (lambda () (display-line-numbers-mode -1)))
+
 ;; The alias to save life.
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (add-to-list 'load-path (concat user-emacs-directory "lisp"))
 
-(require 'init-basic)
-(require 'init-prog)
-(require 'custom-keybindings)
+;; Chopping my fingers.
+(define-key key-translation-map (kbd "ESC") (kbd "C-g"))
+(global-set-key (kbd "C-=") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-s") 'consult-line)
+(global-set-key (kbd "C-x C-b") 'consult-buffer)
+(global-set-key [C-f2] 'vterm-toggle-cd)
+
+;; Keybindings to enter in Multiverse.
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
